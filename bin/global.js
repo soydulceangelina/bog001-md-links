fe90f60e3c48ba2c8842ee801f649fc10697800e
Dulce Mercado <soydulceangelina@gmail.com>
2020-08-25 22:16:05 -0500

    pass local test

--- bin/global.js
+++ bin/global.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
+const process = require('process');
+const clc = require('cli-color');
 const random = require('../src/index');
 
-random.mdLinks();
+const file = process.argv[2];
+
+// el then recibe las respuestaS, iteramos sobre cada respuesta en el forEach
+random.mdLinks(file)
+  .then((responses) => {
+    responses.forEach((response) => {
+      if (response.status === 'fulfilled') {
+        console.log(`${clc.blue(response.value.url)} ${clc.yellow(file)}`);
+      } else {
+        console.log(`${clc.red(response.reason)}`);
+      }
+    });
+  });
--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ const { resolve } = require('path');
 const { extname } = require('path');
 // colores en la consola
 const clc = require('cli-color');
-const process = require('process');
 // mi funcion que ya me trae un arreglo con los links
 const { readingMarkdown } = require('./readingMarkdown');
 
 // dirname es una variable global, representa la ruta donde se ejecuta el proceso
 // (/home/soydulceangelina/bog001-md-links/src/) desde src sale (../) para buscar el README.md.
 // const file = `${__dirname}/../README.md`; // mi path de prueba
-const file = process.argv[2];
 
 const mdLinks = (path) => {
   const toAbsolute = resolve(path);
@@ -30,21 +28,5 @@ const mdLinks = (path) => {
   return Promise.allSettled(promises);// allSettled recibe un arreglo de
   // promesas y devuelve una promesa con las respuestas sean satisfactorias o no
 };
-
-// const validateLinksStatus = () => {
-
-// };
-
-// el then recibe las respuestaS, iteramos sobre cada respuesta en el forEach
-mdLinks(file)
-  .then((responses) => {
-    responses.forEach((response) => {
-      if (response.status === 'fulfilled') {
-        console.log(`${clc.blue(response.value.url)} ${clc.yellow(file)}`);
-      } else {
-        console.log(`${clc.red(response.reason)}`);
-      }
-    });
-  });
 
 module.exports = { mdLinks };
