84dbeb41a64200e27ec16d4a506f75fc683a475a
Dulce Mercado <soydulceangelina@gmail.com>
2020-08-27 20:27:23 -0500

    statistics

--- README.md
+++ README.md
@@ -371,6 +371,7 @@ si tienes dudas existenciales con respecto a estas decisiones. No existe una
 * [Dulce borralo](https://www.flaticon.com/)
 * [Dulce borralo](https://www.soydulceangelina.com/)
 * [Dulce borralo](www.dulceangelina.com)
+* [Dulce borralo](https://www.npmjs.com/packae/cli-color)
 
 ## 9. Checklist
 
--- bin/global.js
+++ bin/global.js
@@ -9,27 +9,82 @@ const mdLinks = require('../src/index');
 const file = process.argv[2];
 const mdLinksInfo = mdLinks(file);
 
-if (process.argv[2] === '') {
+// mdLinksInfo.promise
+//   .then((responses) => {
+//     responses.forEach((element) => {
+//       console.log(element);
+//     });
+//   });
+
+if (process.argv[3] === undefined) {
   mdLinksInfo.infoMd.forEach((mdL) => {
     console.log(`${clc.blue(mdL.getHref)}, ${clc.yellow(mdL.getText)}, ${clc.green(mdL.getFile)}`);
   });
-} else if (process.argv[2] === '--validate') {
+} else if ((process.argv[3] === '--validate' && process.argv[4] === '--stats') || (process.argv[3] === '--stats' && process.argv[4] === '--validate')) {
+  const totalLinks = [];
+  const brokenLinks = [];
+  const failLinks = [];
+  mdLinksInfo.promise
+    .then((responses) => {
+      responses.forEach((response, index) => {
+        const infoLinks = mdLinksInfo.infoMd[index];
+        totalLinks.push(infoLinks.getHref);
+        if (response.status === 'fulfilled') {
+          if (response.value.status >= 200 && response.value.status <= 309) {
+            console.log(`${clc.red.bgWhite('✔')} ${clc.blue(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.magentaBright('Status:', response.value.status)}`);
+          } else if (response.value.status >= 400) {
+            failLinks.push(infoLinks.getHref);
+            console.log(`${clc.red.bgWhite('✘')} ${clc.red(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.magentaBright('Status:', response.value.status)}`);
+          }
+        } else if (response.status === 'rejected') {
+          brokenLinks.push(infoLinks.getHref);
+          console.log(`${clc.red.bgWhite('✘')} ${clc.red(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.red.bgWhite('Status: Error 500')}`);
+        }
+      });
+      // eslint-disable-next-line max-len
+      const uniqueLinks = totalLinks.filter((link) => totalLinks.filter((l) => l === link).length === 1);
+      console.log(`${clc.white.italic.bgBlack('Estadística: Total de liks:')} ${totalLinks.length}`);
+      console.log(`${clc.white.italic.bgBlack('Estadística: Total de liks unicos:')} ${uniqueLinks.length}`);
+      console.log(`${clc.white.italic.bgBlack('Estadística: Total de liks rotos:')} ${brokenLinks.length + failLinks.length}`);
+    });
+} else if (process.argv[3] === '--validate') {
   mdLinksInfo.promise
     .then((responses) => {
       responses.forEach((response, index) => {
         const infoLinks = mdLinksInfo.infoMd[index];
         if (response.status === 'fulfilled') {
-          console.log(`${clc.red.bgWhite('✔')} ${clc.blue(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.magentaBright('Status:', response.value.status)}`);
+          if (response.value.status >= 200 && response.value.status <= 309) {
+            console.log(`${clc.red.bgWhite('✔')} ${clc.blue(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.magentaBright('Status:', response.value.status)}`);
+          } else if (response.value.status >= 400) {
+            console.log(`${clc.red.bgWhite('✘')} ${clc.red(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.magentaBright('Status:', response.value.status)}`);
+          }
         } else if (response.status === 'rejected') {
-          console.log(`${clc.red.bgWhite('✘')} ${clc.red(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}`);
+          console.log(`${clc.red.bgWhite('✘')} ${clc.red(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.red.bgWhite('Status: Error 500')}`);
         }
       });
     });
-  // console.log('lleva --validate');
-} else if (process.argv[2] === '--stats') {
-  console.log('lleva --stats');
-} else if ((process.argv[2] === '--validate' && process.argv[3] === '--stats') || (process.argv[2] === '--stats' && process.argv[3] === '--validate')) {
-  console.log('lleva --stats y --validate');
+} else if (process.argv[3] === '--stats') {
+  const totalLinks = [];
+  mdLinksInfo.promise
+    .then((responses) => {
+      responses.forEach((response, index) => {
+        const infoLinks = mdLinksInfo.infoMd[index];
+        totalLinks.push(infoLinks.getHref);
+        if (response.status === 'fulfilled') {
+          if (response.value.status >= 200 && response.value.status <= 309) {
+            console.log(`${clc.red.bgWhite('✔')} ${clc.blue(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.magentaBright('Status:', response.value.status)}`);
+          } else if (response.value.status >= 400) {
+            console.log(`${clc.red.bgWhite('✘')} ${clc.red(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.magentaBright('Status:', response.value.status)}`);
+          }
+        } else if (response.status === 'rejected') {
+          console.log(`${clc.red.bgWhite('✘')} ${clc.red(infoLinks.getHref)}, ${clc.yellow(infoLinks.getText)}, ${clc.green(infoLinks.getFile)}, ${clc.red.bgWhite('Status: Error 500')}`);
+        }
+      });
+      // eslint-disable-next-line max-len
+      const uniqueLinks = totalLinks.filter((link) => totalLinks.filter((l) => l === link).length === 1);
+      console.log(`${clc.white.italic.bgBlack('Estadística: Total de liks:')} ${totalLinks.length}`);
+      console.log(`${clc.white.italic.bgBlack('Estadística: Total de liks unicos:')} ${uniqueLinks.length}`);
+    });
 }
 
 // el then recibe las respuestaS, iteramos sobre cada respuesta en el forEach
